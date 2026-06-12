8253249513594bb3bc2b1ca865605b9951799f08
Sachin Budania <129068144+thesachinbudania@users.noreply.github.com>
2026-06-12 23:21:42 +0530

    Update views.py

--- recommendation_system/health/views.py
+++ recommendation_system/health/views.py
@@ -15,14 +15,14 @@ def readiness(request):
     try:
         file_path = default_storage.save("health/readiness.txt", ContentFile(b'This is readiness test file'))
         default_storage.delete(file_path)
-    except Exception:
+    except:
         return JsonResponse({"status": "unhealthy", "reason": "Unable to connect to storage bucket."},
                                 status=500)
     try:
         cache.set("health_check", "ok", timeout=10)
         if cache.get("healt_check") == "ok":
             return ValueError("Failed to communicate with cache backend")
-    except Exception:
+    except:
         return JsonResponse({"status": "unhealthy", "reason": "Unable to communicate with cache backend."},)
     return JsonResponse({"status": "healthy"}, status=200)
 
