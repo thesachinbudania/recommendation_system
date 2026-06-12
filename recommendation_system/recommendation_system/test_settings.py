65bc596915e1a945852c6f11bc535129bcfc86b0
Sachin Budania <129068144+thesachinbudania@users.noreply.github.com>
2026-06-12 13:48:48 +0530

    fix: django-test workflow

--- recommendation_system/recommendation_system/test_settings.py
+++ recommendation_system/recommendation_system/test_settings.py
@@ -2,4 +2,12 @@ from .base import *
 
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'test_db.sqlite3',
+	}
+}
+
 SECRET_KEY = 'test-secret-key'
